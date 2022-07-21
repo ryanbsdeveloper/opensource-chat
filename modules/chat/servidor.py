1172b5ae8ecc205355c583bae1943bbf0076dd1b
Ryan Silva <ryanbsdeveloper@gmail.com>
2022-07-21 04:32:57 -0300

    Update servidor.py

--- modules/chat/servidor.py
+++ modules/chat/servidor.py
@@ -10,7 +10,7 @@ class Servidor:
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
 
-        url = f"amqps://ryanl:842684265santos@b-b86d75fd-5111-4c3c-b62c-b999e666760a.mq.us-east-1.amazonaws.com:5671"
+        url = f"url"
         parameters = pika.URLParameters(url)
         parameters.ssl_options = pika.SSLOptions(context=ssl_context)
 
